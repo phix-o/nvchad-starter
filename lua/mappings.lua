cb011d95a255fcb81f6f0e37e2151550a8488093
Phelix <dev.phelixochieng@gmail.com>
2026-09-19 11:04:56 +0300

    feat(mappings): add vscode style comment keyboard shortcuts

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,8 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- comment
+map("n", "<C-_>", "gcc", { desc = "toggle comment", remap = true })
+map("v", "<C-_>", "gc", { desc = "toggle comment", remap = true })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
